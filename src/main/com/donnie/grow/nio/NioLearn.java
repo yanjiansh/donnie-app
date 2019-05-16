dd410121085917898e3a0182324759a0b0194c76
yanjiansh <15601682099@163.com>
2019-05-16 21:41:23 +0800

    Update NIO

---
+++ src/main/com/donnie/grow/nio/NioLearn.java
@@ -0,0 +1,28 @@
+package com.donnie.grow.nio;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioLearn {
+
+    public static void main (String[] args) throws IOException {
+        RandomAccessFile af = new RandomAccessFile("nio-data.txt", "rw");
+        FileChannel fc = af.getChannel();
+        ByteBuffer buf = ByteBuffer.allocate(48);
+        int bytesRead = fc.read(buf);
+        while(bytesRead != -1) {
+            System.out.println("Read " + bytesRead);
+            buf.flip();
+            while(buf.hasRemaining()){
+                System.out.print((char) buf.get());
+            }
+            buf.clear();
+            bytesRead = fc.read(buf);
+        }
+        af.close();
+
+    }
+}
